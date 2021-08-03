b5def8fcd31b7dcf3cb1cdaa44d901e533077a33
Alex <54545559+TreasureMaster@users.noreply.github.com>
2021-08-03 09:16:30 +0300

    ModuleOperation delete

--- views/__init__.py
+++ views/__init__.py
@@ -1,10 +1,10 @@
 from .infomodule import EditableModuleFrame, InfoModuleFrame
-from .operationmoduleframe import ModuleOperation
+# from .operationmoduleframe import ModuleOperation
 from .appwindows import MainWindow, EditWindow, ModuleWindow
 
 
 __all__ = [
-    'ModuleOperation',
+    # 'ModuleOperation',
     'EditableModuleFrame',
     'InfoModuleFrame',
     'MainWindow',
